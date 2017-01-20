48ef6be6c55141cc3da4fdc1d1e2c16a4e1e352b
Joe Cox <joe@factual.com>
2017-01-20 15:37:38 -0800

    Look for not zero shards

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 ES_PORT=9200
 ES_HEALTH_PATH="_cluster/health"
 KOMOREBI_PORT=9400
-KOMOREBI_COMMAND="curl -s localhost:$ES_PORT/$ES_HEALTH_PATH | grep -q green.*\"active_shards\":1"
+KOMOREBI_COMMAND="curl -s localhost:$ES_PORT/$ES_HEALTH_PATH | grep -q green && curl -s localhost:$ES_PORT/$ES_HEALTH_PATH | grep -v -q \"active_shards\":0"
 
 set -e
 
